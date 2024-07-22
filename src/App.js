9cacb6746f884930a256fd0aeb79b67bfa8a01f2
Moksh aggarwal <mokshaggarwal.17@gmail.com>
2024-07-22 16:05:02 +0530

    files restored

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='cont2 w-max'>
+    <div className='cont w-max'>
     
     {/* <Navbar/> */}
     <Navbar/>
