07ca4956083d44f077422ea157f18f2c505482e9
Moksh aggarwal <mokshaggarwal.17@gmail.com>
2024-07-22 16:00:21 +0530

    navbar class change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='cont w-max'>
+    <div className='cont2 w-max'>
     
     {/* <Navbar/> */}
     <Navbar/>
