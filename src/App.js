398932199a21906969f6e19fe24b8b2c222e5e2b
Moksh aggarwal <mokshaggarwal.17@gmail.com>
2024-07-22 15:27:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar/Navbar';
+
+
+function App() {
+  return (
+    <div className='cont w-max'>
+    
+    {/* <Navbar/> */}
+    <Navbar/>
+    
+    
+
+    </div>
+  );
+}
+
+export default App;
